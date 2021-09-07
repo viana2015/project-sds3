c96044708a6085c6087231cec36997caaae9e2c6
Cristiano Viana <cristiano.gvianati@gmail.com>
2021-09-07 15:26:14 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/viana2015" target="_blank" rel="noreferrer">Cristiano Viana</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/cristiano.gviana" target="_blank" rel="noreferrer">@cristiano.gviana</a></small></p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
