4571270ee080092c03fb0d8d0241e5926269413f
Cristiano Viana <cristiano.gvianati@gmail.com>
2021-09-07 21:41:27 -0300

    update logo

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,10 +1,11 @@
-import ImgDsDark from 'assets/img/ds-dark.svg'
+//import ImgDsDark from 'assets/img/ds-dark.svg'
+import ImgSuaLogo from 'assets/img/sua-logo.jpeg'
 const NavBar = () => {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
         <div className="container">
           <nav className="my-2 my-md-0 mr-md-3">
-            <img src={ImgDsDark} alt="DevSuperior" width="120" />
+            <h4 className="text-secundary">Seu Logo</h4>
           </nav>
         </div>
       </div>
