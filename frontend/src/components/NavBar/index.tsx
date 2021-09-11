e250546fad64786944ea3e2b0116aa9c6f15cf7b
Cristiano Viana <cristiano.gvianati@gmail.com>
2021-09-11 12:29:30 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,15 +1,18 @@
-//import ImgDsDark from 'assets/img/ds-dark.svg'
+import ImgDsDark from 'assets/img/ds-dark.svg'
+import { Link } from 'react-router-dom';
 //import ImgSuaLogo from 'assets/img/sua-logo.jpeg'
 const NavBar = () => {
-    return (
-        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
-        <div className="container">
-          <nav className="my-2 my-md-0 mr-md-3">
-            <h4 className="text-secundary">Seu Logo</h4>
-          </nav>
-        </div>
+  return (
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+      <div className="container">
+        <nav className="my-2 my-md-0 mr-md-3">
+          <Link to="/">
+            <img src={ImgDsDark} alt="SuaLogo" width="120" />
+          </Link>
+        </nav>
       </div>
-    );
-  }
-  
-  export default NavBar;
+    </div>
+  );
+}
+
+export default NavBar;
