2d910e4150114e087fd6339d10e35b6eb51a227f
Cristiano Viana <cristiano.gvianati@gmail.com>
2021-09-11 11:49:30 -0300

    Routes

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,5 +1,5 @@
 //import ImgDsDark from 'assets/img/ds-dark.svg'
-import ImgSuaLogo from 'assets/img/sua-logo.jpeg'
+//import ImgSuaLogo from 'assets/img/sua-logo.jpeg'
 const NavBar = () => {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
